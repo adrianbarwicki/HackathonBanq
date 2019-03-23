430c83161757a34ac8723803d7f29903b5de3678
Adrian Barwicki <adrian@vq-labs.com>
2019-03-23 12:52:21 +0100

    items added

--- src/pages/items-list/itemslist.ts
+++ src/pages/items-list/itemslist.ts
@@ -10,6 +10,56 @@ export class ItemsListPage {
 
   public allContacts: any;
 
+  public items = [
+    {
+      name: "Zwarte Bonen",
+      price: 2.28,
+    }, {
+      name: "Frikandelbr",
+      price: 0.81,
+    }, {
+      name: "Tortilla C W",
+      price: 2.25,
+    }, {
+      name: "Sunfloweroil",
+      price: 1.22,
+    }, {
+      name: "AH Sour CR",
+      price: 1.74,
+    }, {
+      name: "GR Sour CR",
+      price: 2.19,
+    }, {
+      name: "GR Jala",
+      price: 2.33,
+    }, {
+      name: "Donuts",
+      price: 3.55,
+    }, {
+      name: "Cheddar Toph",
+      price: 3.77,
+    }, {
+      name: "UIEN",
+      price: 3.14,
+    }, {
+      name: "Avocado",
+      price: 2.99,
+    }, {
+      name: "Ahbio Banaan",
+      price: 0.95,
+    }, {
+      name: "Knoflook",
+      price: 0.81,
+    }, {
+      name: "Koriander",
+      price: 1.74,
+    }, {
+      name: "Rode Pepper",
+      price: 0.39
+    }
+  ];
+
+
   constructor(public navCtrl: NavController, private contacts : Contacts) {
     console.log("test")
   }
