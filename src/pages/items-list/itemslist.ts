f8a32ca6988d86ad8001a562cdd06bf35bfbe9ac
Evert Kors <evert.kors@avanade.com>
2019-03-23 12:10:07 +0100

    Item list

--- src/pages/items-list/itemslist.ts
+++ src/pages/items-list/itemslist.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Contacts, ContactFieldType } from '@ionic-native/contacts/ngx';
 
 @Component({
   selector: 'page-itemslist',
   templateUrl: 'itemslist.html'
 })
 export class ItemsListPage {
-  constructor(public navCtrl: NavController) {
+
+  public allContacts: any;
+
+  constructor(public navCtrl: NavController, private contacts : Contacts) {
+    console.log("test")
   }
+
+  
 
   private goToNextPage() {
     this.navCtrl.push("confirmation");
